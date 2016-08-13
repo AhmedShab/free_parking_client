cf3f9a515a5076bdbde30847874c69809d5d2089
AhmedShab <ahmed.vuw@gmail.com>
2016-08-14 10:53:00 +1200

    working on adding authentication

--- app/router.js
+++ app/router.js
@@ -9,6 +9,8 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('register');
   this.route('find');
+  this.route('login');
+  this.route('protected');
 });
 
 export default Router;
