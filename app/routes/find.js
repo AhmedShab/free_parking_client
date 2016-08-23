c6af6c28523ddb3dbfb44713eacaa1ad0de7ef05
AhmedShab <ahmed.vuw@gmail.com>
2016-08-23 23:03:18 +1200

    search by a single city works display the result

--- app/routes/find.js
+++ app/routes/find.js
@@ -3,7 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   api: Ember.inject.service(),
 
-  model(){
-    return this.get('api').request('/parkings');
+  queryParams: {
+    search: {
+      refreshModel: true
+    }
+  },
+
+  model(params){
+    return this.get('api').request(`/parkings/?${params}`);
   }
 });
